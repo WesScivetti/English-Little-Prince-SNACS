4ab98f286a45b300b4bb1d8db6f6d7865c0f9c0f
Luke Gessler <lukegessler@gmail.com>
2021-12-08 13:29:43 -0500

    add complete data with conllulex conversion

---
+++ legacy/process.py
@@ -0,0 +1,210 @@
+"""
+Combine annotations from the origin c1, c4, c5 pilot annotations and the later annotations for all
+other chapters and produce conllulex for all of them
+"""
+from glob import glob
+from pprint import pprint
+import conllu
+
+def read_conllulex(conllulex_path):
+    """
+    Parse a 19-column .conllulex file.
+    Args:
+        conllulex_path: a filepath to a conllulex file
+    Returns: A list of `conllu.TokenList` for each sentence.
+    `TokenList` is an iterable, and every token is a dictionary keyed by the name
+    of its column, lowercased.
+    """
+    fields = tuple(
+        list(conllu.parser.DEFAULT_FIELDS)
+        + [
+            "smwe",  # 10
+            "lexcat",  # 11
+            "lexlemma",  # 12
+            "ss",  # 13
+            "ss2",  # 14
+            "wmwe",  # 15
+            "wcat",  # 16
+            "wlemma",  # 17
+            "lextag",  # 18
+        ]
+    )
+
+    with open(conllulex_path, "r", encoding="utf-8") as f:
+        return conllu.parse(f.read(), fields=fields)
+
+
+def parse_tsv(path):
+    with open(path, 'r') as f:
+        lines = f.read().split('\n')[1:]
+    output = []
+    for line in lines:
+        pieces = line.split('\t')
+        if line[0] and line[0][0] == '#':
+            output.append({"type": "meta", "line": line})
+        elif pieces[0] and pieces[1]:
+            output.append({
+                "type": "token",
+                "line": line,
+                "id": pieces[0],
+                "form": pieces[1],
+                "ss": pieces[3],
+                "ss2": pieces[4]
+            })
+        else:
+            output.append({"type": "blank"})
+    return output
+
+
+def find_sentences(parsed):
+    sentences = []
+    current = []
+    parsed.append({"type": "blank"})
+
+    for item in parsed:
+        type = item["type"]
+        if type == "blank":
+            if len(current) > 0:
+                sentences.append(current)
+                current = []
+        elif type == "token":
+            current.append(item)
+        else:
+            current.append(item)
+    return sentences
+
+
+def find_mwes(sentences):
+    indices = []
+    for sentence in sentences:
+        runs = {}
+        for i, token in enumerate(sentence):
+            if token["type"] == "token":
+                ss = token["ss"].strip()
+                if len(ss) > 0 and ss != "_" and sentence[i+1]["ss"].strip() == "_":
+                    run = [i]
+                    j = i + 1
+                    while sentence[j]["ss"] == "_":
+                        run.append(j)
+                        j += 1
+                    runs[i] = run
+        indices.append(runs)
+    return indices
+
+
+def fmt(**kwargs):
+    ss = kwargs.get("ss", "_") or "_"
+    ss2 = kwargs.get("ss2", "_") or "_"
+    if ss != "_" and ss[0] != "`": ss = "p." + ss
+    if ss2 != "_" and ss2[0] != "`": ss2 = "p." + ss2
+    return "\t".join([
+        kwargs.get("id"),
+        kwargs.get("form"),
+        kwargs.get("lemma", "_") or "_",
+        kwargs.get("upos", "_") or "_",
+        kwargs.get("xpos", "_") or "_",
+        kwargs.get("feats", "_") or "_",
+        kwargs.get("head", "_") or "_",
+        kwargs.get("deprel", "_") or "_",
+        kwargs.get("deps", "_") or "_",
+        kwargs.get("misc", "_") or "_",
+        kwargs.get("smwe", "_") or "_",
+        kwargs.get("lexcat", "_") or "_",
+        kwargs.get("lexlemma", "_") or "_",
+        ss,
+        ss2,
+        kwargs.get("wmwe", "_") or "_",
+        kwargs.get("wcat", "_") or "_",
+        kwargs.get("wlemma", "_") or "_",
+        kwargs.get("lextag", "_") or "_",
+    ]) + "\n"
+
+
+def format_tsv(path):
+    sentences = find_sentences(parse_tsv(path))
+    mwe_indices = find_mwes(sentences)
+    s = ""
+    for sentence in sentences:
+        i = 0
+        mwe_counter = 1
+        while i < len(sentence):
+            token = sentence[i]
+            if token["type"] == "meta":
+                s += token["line"] + "\n"
+                i += 1
+            elif i not in mwe_indices:
+                s += fmt(
+                    id=token["id"],
+                    form=token["form"],
+                    smwe="_",
+                    lexlemma="_",
+                    ss=token["ss"],
+                    ss2=token["ss2"]
+                )
+                i += 1
+            else:
+                run = mwe_indices[i]
+                for mwe_position, j in enumerate(run):
+                    token = sentence[j]
+                    s += fmt(
+                        id=token["id"],
+                        form=token["form"],
+                        smwe=f"{mwe_counter}:{mwe_position}",
+                        lexlemma="_" if mwe_position == 0 else "_",
+                        ss=token["ss"] if mwe_position == 0 else "_",
+                        ss2=token["ss2"] if mwe_position == 0 else "_"
+                    )
+                mwe_counter += 1
+                i += len(run)
+        s += "\n"
+
+    return s
+
+
+def write_blanked_tsvs():
+    paths = glob('raw_tsv/*.tsv')
+    for path in paths:
+        formatted = format_tsv(path)
+        with open(("blanked" + path[7:-4] + ".conllulex"), 'w') as f:
+            f.write(formatted)
+
+
+def write_blanked_145():
+    paths = glob('raw_145/*.conllulex')
+    combined = ""
+    for path in paths:
+        sents = read_conllulex(path)
+        outpath = f'blanked{path[7:]}'.replace('annotation-chpt', '').replace('.conllulex', '_ADJ.conllulex')
+        with open(outpath, 'w') as f:
+            for sent in sents:
+                del sent.metadata["text"]
+                for token in sent:
+                    token["upos"] = "_"
+                    token["xpos"] = "_"
+                    token["head"] = "_"
+                    token["deprel"] = "_"
+                    token["feats"] = "_"
+                    token["misc"] = "_"
+                    token["deps"] = "_"
+                    token["lexlemma"] = "_"
+                ss = sent.serialize()
+                f.write(ss)
+                combined += ss
+
+
+
+def combine_blanked():
+    write_blanked_145()
+    write_blanked_tsvs()
+    with open('prince_en.conllulex', 'w') as f1:
+        for i in range(1, 28):
+            with open(f'blanked/lpp_{i}_ADJ.conllulex', 'r') as f2:
+                f1.write(f2.read())
+
+
+def main():
+    combine_blanked()
+
+
+if __name__ == '__main__':
+    main()
